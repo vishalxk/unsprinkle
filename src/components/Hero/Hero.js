95e743078ef9780c74955b84a844278e4b545e16
Vishal Kumar Manchala <vishalxk@protonmail.com>
2022-09-06 17:16:59 +0530

    Submit solution to Exercise 3: Accessibility issues

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -21,9 +21,9 @@ const Hero = () => {
             /images/hero-img@3x.jpg 3x
           '
         />
-        <HeroImage src="/images/hero-img.jpg" />
+        <HeroImage alt='Example of photos available on the site (cat on a black' src="/images/hero-img.jpg" />
       </picture>
-      <Swoop src="/swoop.svg" />
+      <Swoop alt='' src="/swoop.svg" />
     </Wrapper>
   );
 };
