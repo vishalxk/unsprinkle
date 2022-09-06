b4721962052a477f3503a25a0e4c4d54a2b601ac
Vishal Kumar Manchala <vishalxk@protonmail.com>
2022-09-06 17:39:17 +0530

    Submit solution to Exercise 4: Tag overflow

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -50,17 +50,23 @@ const Image = styled.img`
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding-block: 4px;
 `;
 
 const Tag = styled.li`
+  display: inline;
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+
+  &:not(:last-of-type) {
+    margin-inline-end: 8px;
+  }
 `;
 
 export default PhotoGridItem;
